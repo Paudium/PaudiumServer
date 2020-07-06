8e752efa3e1fb5ec3f92d784356f6be44b42bfbb
Paudium <paulusjanes116@gmail.com>
2020-07-06 22:58:14 +0300

    final

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
-const { MONGODB } = require('../config');
+const { MONGODB } = require('../config.js');
 
 const pubsub = new PubSub();
 
@@ -16,7 +16,7 @@ const server = new ApolloServer({
 });
 
 mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
+  .connect(MONGODB, { useNewUrlParser: true,useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB Connected');
     return server.listen({ port: PORT });
