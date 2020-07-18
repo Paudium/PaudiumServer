6ebbd63056de826e12d8470f37a0a98ea0bf6692
Paudium <paulusjanes116@gmail.com>
2020-07-18 05:35:34 +0300

    Search function

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // MONGODB: 'mongodb+srv://paulus:paudium_forever@cluster0.alubv.mongodb.net/paudiumdb?retryWrites=true&w=majority',
-    MONGODB:'mongodb://localhost:27017/test',
+    MONGODB: 'mongodb+srv://paulus:paudium_forever@cluster0.alubv.mongodb.net/paudiumdb?retryWrites=true&w=majority',
+    // MONGODB:'mongodb://localhost:27017/test',
     SECRET_KEY: 'some very secret key'
 }
